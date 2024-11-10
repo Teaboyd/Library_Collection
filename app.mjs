1c1e5373788b2e2c11121968d0f6ff2e86891702
Teaboyd <ph.patiphan@kkumail.com>
2024-11-11 04:13:36 +0700

    feat(api):create adding book api and create protect middlewares for validation token , create payload

--- app.mjs
+++ app.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import authRouter from "./app/auths.mjs";
+import authBook from "./app/books.mjs";
 import connectionPool from "./utils/db.mjs";
 
 async function init(){
@@ -10,6 +11,7 @@ async function init(){
 
     app.use(express.json());
     app.use("/auth",authRouter);
+    app.use("/book",authBook);
 
     app.get("/test" , (req,res) => {
     return res.json("Server is working eiei")
--- middlewares/protect.mjs
+++ middlewares/protect.mjs
@@ -1,10 +1,10 @@
 import jwt from 'jsonwebtoken';
 
 
-export const protect = async (req,res) => {
+export const protect = async (req,res,next) => {
         const token = req.headers.authorization
 
-    if(!token || !token.startWith('Bearer')){
+    if(!token || !token.startsWith('Bearer')){
         res.status(401).json({
             message:"Token has invalid format"
         });
@@ -18,6 +18,14 @@ export const protect = async (req,res) => {
                 message: "Token is invalid"
             });
         }
+        
+        req.user_id = payload.id;
+        req.user_id = payload.username;
         next();
     });
-}
+}   
+
+//สร้าง Middleware Protect ไว้ตรวจสอบ Request โดยการเช็คเงื่อนไขสองอย่าง
+//เงื่อนไขแรก คือมีการแนบ Token ไว้ใน Header
+//เงื่อนไขที่สอง คือ Token ที่แนบมานั้นต้อง Valid (ถูกต้อง)
+//เอาข้อมูลที่ได้จาก payload(jwt.sign)มาเก็บไว้ใน req.user_id แล้วเอาไปใช้ใน bookAdd จะได้รู้ว่าใครสร้าง
